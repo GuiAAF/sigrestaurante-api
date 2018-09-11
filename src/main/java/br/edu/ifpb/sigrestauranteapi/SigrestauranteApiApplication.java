7103330d9bdfc2fbd469ac6ff3d570a8eb5578ac
Guilherme <guilhermeifpb@gmail.com>
2018-09-11 13:50:56 -0300

    first commit

---
+++ src/main/java/br/edu/ifpb/sigrestauranteapi/SigrestauranteApiApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifpb.sigrestauranteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigrestauranteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigrestauranteApiApplication.class, args);
+	}
+}
